0c123c80e433e0b0faa711a1678e0b2f25f28961
geoffreylao <geoffreylao@outlook.com>
2020-12-06 14:50:12 -0400

    working with strings

---
+++ beginner_tutorial/working_with_strings/app.py
@@ -0,0 +1,11 @@
+# Working with strings
+
+phrase = "Giraffe"
+print(phrase.upper() + "\n\"Academy\"") # upper() and \ exit function
+print(phrase.lower()) # lowercase function
+print(phrase.isupper()) # check if word is uppercase
+print(phrase.upper().isupper()) # combining uppercase with uppercase check function
+print(len(phrase)) # length of string
+print(phrase[0]) # index of character in string
+print(phrase.index("G")) # find index of character
+print(phrase.replace("fe", "ferig")) # replace string
