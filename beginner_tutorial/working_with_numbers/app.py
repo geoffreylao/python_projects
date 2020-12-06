5a01c2799106922138646f73eeee2ff2d935ca24
geoffreylao <geoffreylao@outlook.com>
2020-12-06 15:52:35 -0400

    working with numbers

---
+++ beginner_tutorial/working_with_numbers/app.py
@@ -0,0 +1,23 @@
+# Working with numbers
+from math import * # import math functions
+
+print(2) # printing int
+print(2.02) # printing decimal
+print(-2) # printing negative
+print(3 + 4.5) # addition
+print(3 * (4 + 5)) # order of operations
+print(10 % 3) # modulus 
+
+my_num = -5
+
+print(my_num) # number variable
+print(str(my_num) + " my favorite number") # convert num to string
+print(abs(my_num)) # absolute value
+print(pow(3, 2)) # 3^2 
+print(max(4,6)) # biggest number
+print(min(4,6)) # smallest number
+print(round(3.2)) # round up/down
+
+print(floor(3.7)) # round down
+print(ciel(3.3)) # round up
+print(sqrt(32)) # square root
