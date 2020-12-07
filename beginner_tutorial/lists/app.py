919ae0835e3faf7b8615683eabd12b562271e986
geoffreylao <geoffreylao@outlook.com>
2020-12-07 13:33:46 -0400

    lists

---
+++ beginner_tutorial/lists/app.py
@@ -0,0 +1,11 @@
+# Lists
+
+friends = ["Kevin", "Karen", "Jim"]
+
+print(friends) # print array
+print(friends[0]) # print index
+print(friends[1:]) # prints everything after and including index
+print(friends[0:1]) # prints range of elements
+
+friends[1] = "Mike"
+print(friends[1]) # reassigned element
