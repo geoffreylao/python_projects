42cb8679e922ed79addbaa4d6c6c5a1962be7185
geoffreylao <geoffreylao@outlook.com>
2020-12-07 14:54:52 -0400

    dictionaries

---
+++ beginner_tutorial/dictionaries/app.py
@@ -0,0 +1,18 @@
+# Dictionaries
+
+monthCoversions = {
+    "Jan": "January",
+    "Feb": "Febuary",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "June",
+    "Jul": "Jul",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December"
+}
+
+print(monthCoversions.get("Dec", "Not a valid Key")) # Value and default value
